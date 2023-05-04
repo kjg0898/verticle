a1d951bb27a802dfbf509b7870cca324218ad2d5
kjg0898 <kjg0898@gmail.com>
2023-05-04 13:38:45 +0900

    img 제외 완성2

---
+++ src/main/java/org/example/config/ksm/KsmConfig.java
@@ -0,0 +1,227 @@
+package org.example.config.ksm;
+
+
+public class KsmConfig {
+    private static final String CONFIG_PATH = "./src/main/resources/config/ksm/testFileConfig";
+
+    private static final String agvConfig  = CONFIG_PATH + "/agvConfig.json";
+    private static final String basisConfig  = CONFIG_PATH + "/basisConfig.json";
+    private static final String bis_detectorConfig  = CONFIG_PATH + "/bis_detectorConfig.json";
+    private static final String bis_detector2Config  = CONFIG_PATH + "/bis_detector2Config.json";
+    private static final String bis_event1Config  = CONFIG_PATH + "/bis_event1Config.json";
+    private static final String bis_event2Config  = CONFIG_PATH + "/bis_event2Config.json";
+    private static final String bis_event3Config  = CONFIG_PATH + "/bis_event3Config.json";
+    private static final String bis_event4Config  = CONFIG_PATH + "/bis_event4Config.json";
+    private static final String bis_event5Config  = CONFIG_PATH + "/bis_event5Config.json";
+    private static final String bis_event6Config  = CONFIG_PATH + "/bis_event6Config.json";
+    private static final String bis_event7Config  = CONFIG_PATH + "/bis_event7Config.json";
+    private static final String bis_iceConfig  = CONFIG_PATH + "/bis_iceConfig.json";
+    private static final String bis_ice2Config  = CONFIG_PATH + "/bis_ice2Config.json";
+    private static final String bis_ice3Config  = CONFIG_PATH + "/bis_ice3Config.json";
+    private static final String chargeConfig  = CONFIG_PATH + "/chargeConfig.json";
+    private static final String controlConfig  = CONFIG_PATH + "/controlConfig.json";
+    private static final String damConfig  = CONFIG_PATH + "/damConfig.json";
+    private static final String detectorConfig  = CONFIG_PATH + "/detectorConfig.json";
+    private static final String eventConfig  = CONFIG_PATH + "/eventConfig.json";
+    // private static final String i2xConfig  = CONFIG_PATH + "/i2xConfig.json";
+    private static final String iceConfig  = CONFIG_PATH + "/iceConfig.json";
+    private static final String ice1Config  = CONFIG_PATH + "/ice1Config.json";
+    private static final String ice2Config  = CONFIG_PATH + "/ice2Config.json";
+    private static final String ice3Config  = CONFIG_PATH + "/ice3Config.json";
+    private static final String ice4Config  = CONFIG_PATH + "/ice4Config.json";
+    private static final String ldmIce10Config  = CONFIG_PATH + "/ldmIce10Config.json";
+    private static final String navy_auto_iceConfig  = CONFIG_PATH + "/navy_auto_iceConfig.json";
+    private static final String navy_controlConfig  = CONFIG_PATH + "/navy_controlConfig.json";
+    private static final String navy_detectorConfig  = CONFIG_PATH + "/navy_detectorConfig.json";
+    private static final String navy_detector2Config  = CONFIG_PATH + "/navy_detector2Config.json";
+    private static final String navy_eventConfig  = CONFIG_PATH + "/navy_eventConfig.json";
+    private static final String navy_mexus_iceConfig  = CONFIG_PATH + "/navy_mexus_iceConfig.json";
+    private static final String pev1Config  = CONFIG_PATH + "/pev1Config.json";
+    private static final String pev2Config  = CONFIG_PATH + "/pev2Config.json";
+    private static final String pev3Config  = CONFIG_PATH + "/pev3Config.json";
+    private static final String pev4Config  = CONFIG_PATH + "/pev4Config.json";
+    private static final String pevConfig  = CONFIG_PATH + "/pevConfig.json";
+    //private static final String result1Config  = CONFIG_PATH + "/result1Config.json";
+   // private static final String rsaConfig  = CONFIG_PATH + "/rsaConfig.json";
+    private static final String saimConfig  = CONFIG_PATH + "/saimConfig.json";
+    //private static final String saomConfig  = CONFIG_PATH + "/saomConfig.json";
+    private static final String sensorConfig  = CONFIG_PATH + "/sensorConfig.json";
+    private static final String test_iceConfig  = CONFIG_PATH + "/test_iceConfig.json";
+    private static final String trafficConfig  = CONFIG_PATH + "/trafficConfig.json";
+
+
+    public static String getAgvConfig() {return agvConfig;}
+
+    public static String getBasisConfig() {
+        return basisConfig;
+    }
+
+    public static String getBisDetectorConfig() {
+        return bis_detectorConfig;
+    }
+
+    public static String getBisDetector2Config() {
+        return bis_detector2Config;
+    }
+
+    public static String getBisEvent1Config() {
+        return bis_event1Config;
+    }
+
+    public static String getBisEvent2Config() {
+        return bis_event2Config;
+    }
+
+    public static String getBisEvent3Config() {
+        return bis_event3Config;
+    }
+
+    public static String getBisEvent4Config() {
+        return bis_event4Config;
+    }
+
+    public static String getBisEvent5Config() {
+        return bis_event5Config;
+    }
+
+    public static String getBisEvent6Config() {
+        return bis_event6Config;
+    }
+
+    public static String getBisEvent7Config() {
+        return bis_event7Config;
+    }
+
+    public static String getBisIceConfig() {
+        return bis_iceConfig;
+    }
+
+    public static String getBisIce2Config() {
+        return bis_ice2Config;
+    }
+
+    public static String getBisIce3Config() {
+        return bis_ice3Config;
+    }
+
+    public static String getChargeConfig() {
+        return chargeConfig;
+    }
+
+    public static String getControlConfig() {
+        return controlConfig;
+    }
+
+    public static String getDamConfig() {
+        return damConfig;
+    }
+
+    public static String getDetectorConfig() {
+        return detectorConfig;
+    }
+
+    public static String getEventConfig() {
+        return eventConfig;
+    }
+
+ /*   public static String getI2xConfig() {
+        return i2xConfig;
+    }*/
+
+    public static String getIceConfig() {
+        return iceConfig;
+    }
+
+    public static String getIce1Config() {
+        return ice1Config;
+    }
+
+    public static String getIce2Config() {
+        return ice2Config;
+    }
+
+    public static String getIce3Config() {
+        return ice3Config;
+    }
+
+    public static String getIce4Config() {
+        return ice4Config;
+    }
+
+    public static String getLdmIce10Config() {
+        return ldmIce10Config;
+    }
+
+    public static String getNavy_auto_iceConfig() {
+        return navy_auto_iceConfig;
+    }
+
+    public static String getNavy_controlConfig() {
+        return navy_controlConfig;
+    }
+
+    public static String getNavy_detectorConfig() {
+        return navy_detectorConfig;
+    }
+
+    public static String getNavy_detector2Config() {
+        return navy_detector2Config;
+    }
+
+    public static String getNavy_eventConfig() {
+        return navy_eventConfig;
+    }
+
+    public static String getNavy_mexus_iceConfig() {
+        return navy_mexus_iceConfig;
+    }
+
+    public static String getPev1Config() {
+        return pev1Config;
+    }
+
+    public static String getPev2Config() {
+        return pev2Config;
+    }
+
+    public static String getPev3Config() {
+        return pev3Config;
+    }
+
+    public static String getPev4Config() {
+        return pev4Config;
+    }
+
+    public static String getPevConfig() {
+        return pevConfig;
+    }
+
+  /*  public static String getResult1Config() {
+        return result1Config;
+    }*/
+
+   /* public static String getRsaConfig() {
+        return rsaConfig;
+    }*/
+
+    public static String getSaimConfig() {
+        return saimConfig;
+    }
+
+    /*public static String getSaomConfigVV() {
+        return saomConfig;
+    }*/
+
+    public static String getSensorConfig() {
+        return sensorConfig;
+    }
+
+    public static String getTest_iceConfig() {
+        return test_iceConfig;
+    }
+
+    public static String getTrafficConfig() {
+        return trafficConfig;
+    }
+
+}
